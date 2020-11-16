61875b5ec485ff2ebe1c57d7344981d80ad2a105
Khem Holden <khrholde@ucsc.edu>
2020-11-15 23:13:53 -0800

    fixing the issue where image only channels were doubled in the txt file, also made it so that the file isnt read every time inc ase its a long file

--- curatorMain.py
+++ curatorMain.py
@@ -1,37 +1,34 @@
 import discord
 import random
-from discord import Game
 from discord.ext.commands import Bot
+import os
 
-BOT_PREFIX = ("s!", "!", "s! ")
-
+BOT_PREFIX = ("c!")
+TOKEN = 
 client = Bot(command_prefix=BOT_PREFIX)
-
-#list of channel ids
+folder=os.path.dirname(os.path.realpath(__file__)) 
 def get_channel_list():
-	channelfile = open("channellist.txt","r")
+	channelfile = open(os.path.join(folder,'channellist.txt'),"r")
 	imageonlychannels = channelfile.read().splitlines()
 	channelfile.close()
 	return imageonlychannels
+imageonlylist = get_channel_list()
 
 @client.event
 async def on_ready():
-	#prints out the bot used to see if its running
+
 	print(f'Logged in as {client.user}')
 
-#set the channel to image only
 @client.command('imageonly')
 async def imageonly(message):
 	channel = message.channel
-	imageonlychannels = get_channel_list()
-	if(imageonlychannels):
-		if(str(channel.id) in imageonlychannels):
+	global imageonlylist
+	if(imageonlylist):
+		if(str(channel.id) in imageonlylist):
 			def checkauthor(m2):
 				return message.author ==m2.author
 			await channel.send("Image only disabled")
-			channelfile = open("channellist.txt","r")
-			tempchannellist=channelfile.read().splitlines()
-			channelfile.close()
+			tempchannellist=get_channel_list()
 			tempchannellist.remove(str(channel.id))
 			channelfile=open("channellist.txt", "w")
 			if(len(tempchannellist)==0):
@@ -39,58 +36,73 @@ async def imageonly(message):
 			else:
 				channelfile.writelines(["%s\n" % item  for item in tempchannellist])
 			channelfile.close()
-			imageonlychannels=tempchannellist
-			await message.message.delete()
+			imageonlylist=tempchannellist
+			print(imageonlylist)
 		else:
 			def checkauthor(m2):
 				return message.author ==m2.author
 			await channel.send("Image only enabled")
 			channelid = channel.id
-			imageonlychannels.append(channelid)
-			channelfile = open("channellist.txt","w")
-			channelfile.writelines(["%s\n" % item  for item in imageonlychannels])
+			imageonlylist.append(str(channel.id))
+			print(imageonlylist)
+			channelfile = open("channellist.txt","a")
+			channelfile.writelines(["%s\n" % channelid])
 			channelfile.close()
-
 			await message.message.delete()
-			#channelfile = open("channellist.txt","w+")
-			#await client.get_channel(imageonlychannels[0]).send("testing saving")
-			channelid = channel.id
-			imageonlychannels.append(channelid)
-			channelfile = open("channellist.txt","w")
-			channelfile.writelines(["%s\n" % item  for item in imageonlychannels])
-			channelfile.close()
+
 	#repeated code for lower level if statement
 	else:
 		def checkauthor(m2):
 			return message.author ==m2.author
 		await channel.send("Image only enabled")
 		channelid = channel.id
-		imageonlychannels.append(channelid)
+		imageonlylist.append(str(channel.id))
 		channelfile = open("channellist.txt","w")
-		channelfile.writelines(["%s\n" % item  for item in imageonlychannels])
+		channelfile.writelines(["%s\n" % item  for item in imageonlylist])
 		channelfile.close()
 		await message.message.delete()
+		
 		#channelfile = open("channellist.txt","w+")
 		#await client.get_channel(imageonlychannels[0]).send("testing saving")
 		
-#check if a message is sent in image only channel and delete it if it is
 @client.event
 async def on_message(message):
 	await client.process_commands(message)
 	channel = message.channel
-	imageonlychannels = get_channel_list()
-	if(str(channel.id) in imageonlychannels and message.author != client.user):
-		if (message.attachments):
-			pass
-		elif(len(message.content) > 8):
-			if(message.content[0:8] == 'https://'):
+	global imageonlylist
+	if (not message.author.bot):
+		if(str(channel.id) in imageonlylist and message.author != client.user):
+			if (message.attachments):
 				pass
+			elif(len(message.content) > 8):
+				if(message.content[0:8] == 'https://'):
+					pass
+				else:
+					try:
+						await message.delete()
+					#message was already deleted
+					except discord.errors.NotFound:
+						pass
 			else:
-				await message.delete()
-		else:
-			await message.delete()
+				try:
+					await message.delete()
+				#message was already deleted
+				except discord.errors.NotFound:
+					pass
 		
 
+async def list_servers():
+	await client.wait_until_ready()
+	while not client.is_closed:
+		print("Current servers:")
+		for server in client.servers:
+			print(server.name)
+		await asyncio.sleep(600)
 
-client.run(TOKEN)
-#client.close()
+try:
+	client.loop.create_task(list_servers())
+	client.run(TOKEN)
+except:
+	channelfile = open("channellist.txt","w")
+	channelfile.writelines(["%s\n" % item  for item in imageonlylist])
+	channelfile.close()
